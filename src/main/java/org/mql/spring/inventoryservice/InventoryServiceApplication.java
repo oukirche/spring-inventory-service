d9248adafc5b058eb129adf8a50ebc2916de6eab
Abdessamade OUKIRCHE <aoukirche@sii.ma>
2021-04-22 13:38:38 +0000

    add some features

--- src/main/java/org/mql/spring/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/org/mql/spring/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package org.mql.spring.inventoryservice;
 
+import org.mql.spring.inventoryservice.dao.ProductRepository;
+import org.mql.spring.inventoryservice.entities.Product;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +15,15 @@ public class InventoryServiceApplication {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+		return args -> {
+			restConfiguration.exposeIdsFor(Product.class);
+			productRepository.save(new Product(null,"HP",5000));
+			productRepository.save(new Product(null,"DELL",6700));
+			productRepository.save(new Product(null,"SONY",17000));
+
+
+		};
+	}
 }
---
+++ src/main/java/org/mql/spring/inventoryservice/entities/Product.java
@@ -0,0 +1,24 @@
+package org.mql.spring.inventoryservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long productId;
+    private String name;
+    private double price;
+}
